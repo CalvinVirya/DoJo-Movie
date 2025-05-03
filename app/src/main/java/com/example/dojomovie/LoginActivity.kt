42e6428186d703be6bbe4657603bc0826b0f28cc
Joseeul <jose_andreas@icloud.com>
2025-05-03 20:44:10 +0700

    login

--- app/src/main/java/com/example/dojomovie/LoginActivity.kt
+++ app/src/main/java/com/example/dojomovie/LoginActivity.kt
@@ -1,20 +1,28 @@
 package com.example.dojomovie
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var ivLogo: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        ivLogo = findViewById(R.id.ivLogo)
+
+        val assetManager = assets
+        val inputStream = assetManager.open("logo.png")
+        val drawable = Drawable.createFromStream(inputStream, null)
+        ivLogo.setImageDrawable(drawable)
+        ivLogo.background = null
     }
 }
