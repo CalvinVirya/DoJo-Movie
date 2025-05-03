382900a14b21ce01e0d87cbd34ea93a8e5218d4a
CalvinVirya <calvinviryayunardy@gmail.com>
2025-05-03 15:50:27 +0700

    landing done

--- app/src/main/java/com/example/dojomovie/MainActivity.kt
+++ app/src/main/java/com/example/dojomovie/MainActivity.kt
@@ -1,15 +1,37 @@
 package com.example.dojomovie
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnExplore: Button
+    lateinit var ivIntro: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnExplore = findViewById(R.id.btnExplore)
+        ivIntro = findViewById(R.id.ivIntro)
+
+        val assetManager = assets
+        val inputStream = assetManager.open("intro.png")
+        val drawable = Drawable.createFromStream(inputStream, null)
+        ivIntro.setImageDrawable(drawable)
+        ivIntro.background = null
+
+        btnExplore.setOnClickListener {
+            var intent = Intent(MainActivity@this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
